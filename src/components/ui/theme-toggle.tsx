d2472cf4473d9e76726c8655998c31c53dd68add
Guilherme D'Alessandro <guikemon2@gmail.com>
2024-04-06 01:22:57 -0300

    fix: build failing

--- src/components/ui/theme-toggle.tsx
+++ src/components/ui/theme-toggle.tsx
@@ -13,7 +13,7 @@ import {
 } from '@/components/ui/dropdown-menu';
 
 type Props = {
-  buttonProps: ButtonProps | undefined;
+  buttonProps?: ButtonProps | undefined;
 };
 
 export function ThemeToggle(props: Props) {
