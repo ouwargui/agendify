0bfcea7d72918f96cac4d77b1c6b218a4882e7ea
ouwargui <guikemon2@gmail.com>
2024-04-18 01:52:25 -0300

    feat: activate sidebar items based on pathname

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -6,36 +6,41 @@ import {
   TooltipTrigger,
 } from '@/components/ui/tooltip';
 import {cn} from '@/lib/shadcn/utils';
-import {
-  Home,
-  LineChart,
-  Package,
-  Package2,
-  Settings,
-  ShoppingCart,
-  Users2,
-} from 'lucide-react';
+import {CalendarDays, Home, LineChart, Settings, Users2} from 'lucide-react';
 import Link from 'next/link';
+import {usePathname} from 'next/navigation';
 
 export function Sidebar() {
+  const pathname = usePathname();
+
+  function isActive(path: string) {
+    return pathname === path;
+  }
+
   return (
     <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
-      <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
+      <nav className="flex flex-col items-center px-2 sm:py-5">
         <Link
           href="#"
-          className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
+          className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-lg bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
         >
-          <Package2 className="h-4 w-4 transition-all group-hover:scale-110" />
+          <CalendarDays className="h-4 w-4 transition-all group-hover:scale-110" />
           <span className="sr-only">Agendify</span>
         </Link>
+      </nav>
+      <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
         <TooltipProvider delayDuration={0}>
           <Tooltip>
             <TooltipTrigger asChild>
               <Link
-                href="#"
+                href="/dashboard"
                 className={cn(
                   'flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8',
-                  {'bg-accent': false, 'text-muted-foreground': false},
+                  {
+                    'bg-primary': isActive('/dashboard'),
+                    'text-primary-foreground': isActive('/dashboard'),
+                    'rounded-full': isActive('/dashboard'),
+                  },
                 )}
               >
                 <Home className="h-5 w-5" />
@@ -49,44 +54,14 @@ export function Sidebar() {
           <Tooltip>
             <TooltipTrigger asChild>
               <Link
-                href="#"
+                href="/dashboard/customers"
                 className={cn(
                   'flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8',
-                  {'bg-accent': false, 'text-muted-foreground': false},
-                )}
-              >
-                <ShoppingCart className="h-5 w-5" />
-                <span className="sr-only">Orders</span>
-              </Link>
-            </TooltipTrigger>
-            <TooltipContent side="right">Orders</TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
-        <TooltipProvider delayDuration={0}>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Link
-                href="#"
-                className={cn(
-                  'flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8',
-                  {'bg-accent': false, 'text-muted-foreground': false},
-                )}
-              >
-                <Package className="h-5 w-5" />
-                <span className="sr-only">Products</span>
-              </Link>
-            </TooltipTrigger>
-            <TooltipContent side="right">Products</TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
-        <TooltipProvider delayDuration={0}>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Link
-                href="#"
-                className={cn(
-                  'flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8',
-                  {'bg-accent': false, 'text-muted-foreground': false},
+                  {
+                    'bg-primary': isActive('/dashboard/customers'),
+                    'text-primary-foreground': isActive('/dashboard/customers'),
+                    'rounded-full': isActive('/dashboard/customers'),
+                  },
                 )}
               >
                 <Users2 className="h-5 w-5" />
@@ -100,10 +75,14 @@ export function Sidebar() {
           <Tooltip>
             <TooltipTrigger asChild>
               <Link
-                href="#"
+                href="/dashboard/analytics"
                 className={cn(
                   'flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8',
-                  {'bg-accent': false, 'text-muted-foreground': false},
+                  {
+                    'bg-primary': isActive('/dashboard/analytics'),
+                    'text-primary-foreground': isActive('/dashboard/analytics'),
+                    'rounded-full': isActive('/dashboard/analytics'),
+                  },
                 )}
               >
                 <LineChart className="h-5 w-5" />
@@ -119,10 +98,14 @@ export function Sidebar() {
           <Tooltip>
             <TooltipTrigger asChild>
               <Link
-                href="#"
+                href="/dashboard/settings"
                 className={cn(
                   'flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8',
-                  {'bg-accent': false, 'text-muted-foreground': false},
+                  {
+                    'bg-primary': isActive('/dashboard/settings'),
+                    'text-primary-foreground': isActive('/dashboard/settings'),
+                    'rounded-full': isActive('/dashboard/settings'),
+                  },
                 )}
               >
                 <Settings className="h-5 w-5" />
